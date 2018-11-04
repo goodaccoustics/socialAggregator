661fa25df7c9f9956f58b8384f524c7ffb3ee332
goodaccoustics <goodaccoustics@hotmail.com>
2018-11-04 15:03:45 +0000

    commit

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 var createError = require('http-errors');
-var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var authRouter = require('./routes/auth');
+//var errorRouter = require('./routes/error');
+// added by Justin Lim
+var express = require('express');
 var app = express();
-
+var passport = require('passport');
+var session = require('express-session');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+passport.use(new GoogleStrategy({
+	clientID: '609318490070-2bau1106rjghkh3mp21npps6oi15ruk9.apps.googleusercontent.com',
+	clientSecret: 'TjeiDLZPtDzX3lOD7AYZuaF7',
+	callbackURL: 'http://localhost:3000/auth/google/callback'
+	},
+	function(req, accessToken, refreshToken, profile, done){
+		// set up user in your app database
+		done(null, profile);
+	}
+));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,9 +34,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//jl for passport
+app.use(session({secret: 'anything'}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(function(user, done){
+	done(null, user);
+}); //place user object into the session
+passport.deserializeUser(function(user, done){
+	done(null, user);
+});
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/auth', authRouter);
+//app.user('/error', errorRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+
+router.route('/google/callback')
+	.get(passport.authenticate('google', {
+		successRedirect: '/users/',
+		failure: '/error/'
+}));
+
+
+router.route('/google')
+	.get(passport.authenticate('google', {
+		scope: [
+		'profile',
+		'email'
+		]
+}));
+
+module.exports = router;
